86e60a48e2039975e501133e9c6ebe5fdfa80d2c
rocksoftcode <rocky@rocksoftcode.com>
2022-12-02 09:56:38 -0600

    Day 2

--- day2/day2-1.groovy
+++ day2/day2-1.groovy
@@ -2,4 +2,4 @@ def score = [A: 1, B: 2, C: 3]
 def lookup = [X: 'A', Y: 'B', Z: 'C']
 def win = [A: 'C', B: 'A', C: 'B']
 def input = new File('input.txt').text.split(/\n/)*.split(/\s/)
-println input.sum { (lookup[it[1]] == it[0] ? 3 : (it[0] == win[lookup[it[1]]] ? 6 : 0)) + score[lookup[it[1]]] }
+println input.sum {(lookup[it[1]] == it[0] ? 3 : (it[0] == win[lookup[it[1]]] ? 6 : 0)) + score[lookup[it[1]]]}
