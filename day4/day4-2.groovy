b8c874930583952693b220b85da9b1980575a6d9
rocksoftcode <rocky@rocksoftcode.com>
2022-12-04 11:56:57 -0600

    Day 4

--- day4/day4-2.groovy
+++ day4/day4-2.groovy
@@ -2,4 +2,4 @@ def input = new File('input.txt').text.split(/\n/)*.split(',')*.collect {
 	def bounds = it.split('-')*.toInteger()
 	(bounds[0]..bounds[1]).toList()
 }
-println input.count {it[0].intersect(it[1]) }
+println input.count {it[0].intersect(it[1])}
