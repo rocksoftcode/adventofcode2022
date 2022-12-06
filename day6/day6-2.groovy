a948a4f6a519ca9fb437ae14eab8f4024c119a76
rocksoftcode <rocky@rocksoftcode.com>
2022-12-06 08:55:31 -0600

    Day 6

--- day6/day6-2.groovy
+++ day6/day6-2.groovy
@@ -1,4 +1,4 @@
 def input = new File('input.txt').text.split('').toList()
 def i = 0
-while (i++ < input.size() - 4) if (input[i..i+3].unique().size() == 4) break
-println i + 4
+while (i++ < input.size() - 14) if (input[i..i+13].unique().size() == 14) break
+println i + 14
