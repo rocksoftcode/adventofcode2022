b22126b2f610586ddfe13664b4ce767b76f3fbe5
rocksoftcode <rocky@rocksoftcode.com>
2022-12-07 14:07:26 -0600

    Day 7

--- day7/day7-1.groovy
+++ day7/day7-1.groovy
@@ -5,13 +5,9 @@ def encode = {it.join('/').replaceAll('//', '/')}
 input.each {
 	def line = it.split(/\s/)
 	if (line[0] == '$') {
-		if (line[1] == 'cd' && line[2] == '..') {
-			trail.removeLast()
-		} else if (line[1] == 'cd') {
-			trail << line[2]
-		} else if (line[1] == 'ls') {
-			contents[encode(trail)] = 0
-		}
+		if (line[1] == 'cd' && line[2] == '..') trail.removeLast()
+		else if (line[1] == 'cd') trail << line[2]
+		else if (line[1] == 'ls') contents[encode(trail)] = 0
 	} else if (line[0].isNumber()) {
 		def crawl = trail.collect()
 		while (crawl) {
