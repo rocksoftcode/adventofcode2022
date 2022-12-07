3e281da997f6d695f272a42d8ecd4219a9075448
rocksoftcode <rocky@rocksoftcode.com>
2022-12-07 10:46:45 -0600

    Day 7

---
+++ day7/day7-1.groovy
@@ -0,0 +1,23 @@
+def input = new File('input.txt').text.split(/\n/)
+def trail = []
+def contents = [:]
+def encode = {it.join('/').replaceAll('//', '/')}
+input.each {
+	def line = it.split(/\s/)
+	if (line[0] == '$') {
+		if (line[1] == 'cd' && line[2] == '..') {
+			trail.removeLast()
+		} else if (line[1] == 'cd') {
+			trail << line[2]
+		} else if (line[1] == 'ls') {
+			contents[encode(trail)] = 0
+		}
+	} else if (line[0].isNumber()) {
+		def crawl = trail.collect()
+		while (crawl) {
+			contents[encode(crawl)] += line[0].toInteger()
+			crawl.removeLast()
+		}
+	}
+}
+println contents.entrySet()*.value.findAll { it <= 100000 }.sum()
