1dc7e11fa58dce23ae1c9984303ff2ae6a4b80dc
rocksoftcode <rocky@rocksoftcode.com>
2022-12-10 14:07:32 -0600

    Day 10

--- day10/day10-2.groovy
+++ day10/day10-2.groovy
@@ -8,7 +8,7 @@ input.each {it ->
 	} else cycle++
 }
 def x = 1
-def corr = {i -> i < 40 ? i - 1 : (i % 40) - 1}
+def corr = {i -> (i % 40) - 1}
 (1..240).each {
 	x += ops[it] ? ops[it] : 0
 	print corr(it) in (x - 1..x + 1) ? '#' : '.'
